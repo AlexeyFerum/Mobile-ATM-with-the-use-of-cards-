b15e7634b5c409722dd9fd71309849807b0fd38c
Alex Ferum <mr.alex.zhelferum@gmail.com>
2019-05-30 01:24:25 +0300

    Test

---
+++ MobileATM_Server_Library/MobileATM_Server_Library/Client.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace MobileATM_Library
+{
+    public class Client
+    {
+        private string name;
+        private string surname;
+        private List<Account> accountList;
+
+        public string Name
+        {
+            get => name;
+
+            private set => name = value;
+        }
+
+        public string Surname
+        {
+            get => surname;
+
+            private set => surname = value;
+        }
+
+        public Client(string name, string surname)
+        {
+            accountList = new List<Account>();
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public void addAccount() { }
+    }
+}

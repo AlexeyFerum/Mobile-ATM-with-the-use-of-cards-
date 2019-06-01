93e907cf911ae7598b103ad0ba31c94127d3bc8f
Alex Ferum <mr.alex.zhelferum@gmail.com>
2019-06-01 13:09:06 +0300

    Adding Db

---
+++ MobileATM_Server_Library/Connect/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Common;
+
+namespace Connect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string provider = ConfigurationManager.AppSettings["provider"];
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if (connection == null)
+                {
+                    Console.WriteLine("Connection error");
+                    Console.ReadLine();
+                    return;
+                }
+
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                DbCommand command = factory.CreateCommand();
+
+                if (command == null)
+                {
+                    Console.WriteLine("Command error");
+                    Console.ReadLine();
+                    return;
+                }
+
+                command.Connection = connection;
+
+                command.CommandText = "Select * from ServiceStaff";
+
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    int i = 0;
+
+                    while (dataReader.Read())
+                    {
+                        while (i < dataReader.FieldCount)
+                        {
+                            Console.WriteLine($"{dataReader[i]}");
+                            i++;
+                        }
+                    }
+                }
+
+                connection.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
